e4bb4fa0c1d579c46bcfc0346e2e4a769ccc040d
Elvis Maliza <emaliza2482@gmail.com>
2018-11-03 17:06:45 +0000

    edits

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 // JavaScript File
 /*global $*/
 
-$('body').click(function(){
+$('button').click(function(){
     
 });
 
+$("#meredith").click(function(){
+    $(".galleryPic").attr("src", "https://media.giphy.com/media/10vtLafTnJYLoA/giphy.gif");
+
+});
+
+$("#derek").click(function(){
+    $(".galleryPic").attr("src", "https://media1.tenor.com/images/108720e24fe951c18d14a864cec6c5ab/tenor.gif?itemid=5155415");
+
+});
+ $("#izzie").click(function(){
+    $(".galleryPic").attr("src", "https://media1.tenor.com/images/9e0e9f4da8da1424e4b9988b865d93ab/tenor.gif?itemid=5778010");
+
+});
+ $("#christina").click(function(){
+    $(".galleryPic").attr("src", "https://media.rbl.ms/image?u=%2Ffiles%2F2016%2F05%2F29%2F636000875065676497-1715062777_3lsms4q.gif&ho=https%3A%2F%2Faz616578.vo.msecnd.net&s=67&h=62d71280047120352cfa9fdb7090bb9bc8e8619253e7db1825432f274b780c55&size=980x&c=106136273");
+
+});
+
+$("#karev").click(function(){
+    $(".galleryPic").attr("src", "https://66.media.tumblr.com/351a4dc62c5fc23371a39c04b422ffcf/tumblr_inline_p7gxpvCjyq1rrrkde_500.gif");
+
+});
+
+$("#george").click(function(){
+    $(".galleryPic").attr("src", "https://i.gifer.com/EO0l.gif");
+
+});
